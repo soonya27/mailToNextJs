d91b34635bb4637b73187c42cb56762de47e7b8f
PyeonSoyeong <psy@daedamo.com>
2025-05-21 13:49:12 +0900

    eslint 수정

--- src/app/components/ui/icons/LinkIcon.tsx
+++ src/app/components/ui/icons/LinkIcon.tsx
@@ -1,11 +1,9 @@
-import React from 'react';
+import React from "react";
 import { FaLink } from "react-icons/fa";
-import { IoReturnDownForwardSharp } from "react-icons/io5";
 export default function LinkIcon() {
-    return (
-        <>
-            <FaLink />
-        </>
-    );
+  return (
+    <>
+      <FaLink />
+    </>
+  );
 }
-
